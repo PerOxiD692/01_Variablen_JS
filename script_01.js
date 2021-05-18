e28a5f0cbf3cb83629c29bc1bc7c59434a9d2ef3
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-18 11:35:42 +0200

    Deklaration, Wertzuweisung, Ausgabe

--- script_01.js
+++ script_01.js
@@ -1,1 +1,21 @@
 /***** Variablen 01 *******/
+
+// Das ist ein Zeilen-Kommentar
+
+/**
+ *  Block-Kommentar
+ */
+
+// Ausgabe
+//console.log("Hallo Welt!");
+//console.log(FirstName); 
+
+// Deklaration + Wertzuweisung + Ausgabe
+/*
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+let familyName = "Mustermann";  // Deklaration + Wertzuweisung
+console.log(familyName); // Ausgabe
+console.log(firstName + " " + familyName);
+*/
